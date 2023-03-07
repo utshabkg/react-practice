76966b1dcdbd78ea4af78057a289a12e073e6add
utshabkg <ranjanyouth@gmail.com>
2023-03-07 19:47:30 +0600

    increment and decrement with useState Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import "./App.css";
 import Header from "./components/Header";
-import Movie from "./components/Movie";
-import movies from "./movie.json";
+// import Movie from "./components/Movie";
+// import movies from "./movie.json";
+import React, { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(1);
+  function increment() {
+    setNum(num + 1);
+  }
+  function decrement() {
+    setNum(num - 1);
+  }
   return (
     <div className="App">
       <Header />
       <div className="main">
-        {movies.map((element, index) => {
-          return (
+        <h1 className="heading">{num}</h1>
+        <div className="buttons">
+          <button className="btn" onClick={increment}>
+            increment
+          </button>
+          <button className="btn" onClick={decrement}>
+            decrement
+          </button>
+        </div>
+        {/* {movies.map((element, index) => {
+          return ( 
             <Movie
               key={index}
               title={element.Title}
@@ -17,7 +34,7 @@ function App() {
               img={element.Poster}
             />
           );
-        })}
+        })} */}
       </div>
     </div>
   );
