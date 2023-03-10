8682b8b7fe1e2a8d9e100ae81ceddb68024309cd
utshabkg <ranjanyouth@gmail.com>
2023-03-10 13:16:11 +0600

    added routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 import Counter from "./components/counter/Counter";
 import Demo from "./components/useEffect/Demo";
 import Movie from "./components/movie/Movie";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Counter />
-      <Demo />
-      <Movie />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/movie" element={<Movie />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
