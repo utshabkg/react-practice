a4f0b3b4e545bb72e451d4373f702d6e54f4b2ab
utshabkg <ranjanyouth@gmail.com>
2023-03-10 13:37:17 +0600

    404 page added.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Counter from "./components/counter/Counter";
 import Demo from "./components/useEffectDemo/Demo";
 import Movie from "./components/movie/Movie";
 import About from "./components/about/About";
+import Error from "./components/error/Error";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/counter" element={<Counter />} />
           <Route path="/employees" element={<Demo />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/error/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <div>404. Page not Found!</div>;
+};
+
+export default Error;
