8d23c59eedba0a7c73f40714feeb28f8ba01758f
utshabkg <ranjanyouth@gmail.com>
2023-03-10 15:12:31 +0600

    added navigation to previous page if 404.

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
-  return <div>404. Page not Found!</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Fragment>
+      <div>404. Page not Found!</div>
+      <button onClick={() => navigate(-1)}>Go to Previous Page</button>
+    </Fragment>
+  );
 };
 
 export default Error;
