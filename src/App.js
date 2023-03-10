d4cdd956712bd8fc3f3c01529ae43b45cbc822b4
utshabkg <ranjanyouth@gmail.com>
2023-03-10 15:59:58 +0600

    employee info transferred to homepage.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Movie from "./components/movie/Movie";
 import Counter from "./components/counter/Counter";
-import Demo from "./components/useEffectDemo/Demo";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Insta from "./components/contact/Insta";
@@ -22,7 +21,6 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/movie" element={<Movie />} />
           <Route path="/counter" element={<Counter />} />
-          <Route path="/employees" element={<Demo />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />}>
             <Route index element={<Mail />} />
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,44 @@
-import React from "react";
+import "./home.scss";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const [state, setState] = useState(2);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(
+        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
+      );
+      const response = await get.json();
+      setData(response);
+      console.log(response);
+    }
+
+    getData();
+  }, [state]);
+  return (
+    <div className="demo">
+      <h2>Demo Employee List [Showing {state}]</h2>
+      <div className="data" style={{ color: "yellow" }}>
+        <h4>First Name</h4>
+        <h4>Last Name</h4>
+        <h4>Email</h4>
+      </div>
+      {data.map((element, index) => {
+        return (
+          <div key={index} className="data">
+            <h4>{element.firstName}</h4>
+            <h4>{element.lastName}</h4>
+            <h4>{element.email}</h4>
+          </div>
+        );
+      })}
+      <button onClick={() => setState(state + 2)} className="btn-fetch">
+        Fetch 2 more entries
+      </button>
+    </div>
+  );
 };
 
 export default Home;
