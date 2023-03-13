65773c50c6bc6eeb30836fa78f7c7a3b261812ee
utshabkg <ranjanyouth@gmail.com>
2023-03-13 18:10:19 +0600

    added value and focus with button.

--- src/components/hooksDemo/useRefDemo.js
+++ src/components/hooksDemo/useRefDemo.js
@@ -2,19 +2,27 @@ import React, { useState, useEffect, useRef } from "react";
 
 const UseRefDemo = () => {
   const [input, setInput] = useState("");
-  const prevStat = useRef("");
+  const inputField = useRef();
+  // const prevState = useRef("");
 
-  useEffect(() => {
-    prevStat.current = input;
-  }, [input]);
+  // useEffect(() => {
+  //   prevStat.current = input;
+  // }, [input]);
 
   const formHandler = (e) => {
     setInput(e.target.value);
   };
+  const clickHandler = () => {
+    inputField.current.value = "Utshab";
+    inputField.current.focus();
+  };
   return (
     <div>
-      <input value={input} onChange={formHandler} />
-      <h4>Previous Stat was: {prevStat.current}</h4>
+      {/* <input value={input} onChange={formHandler} /> */}
+      {/* <h4>Previous Stat was: {prevState.current}</h4> */}
+      <input ref={inputField} value={input} onChange={formHandler} />
+      <button onClick={clickHandler}>Click Me to Focus inside TextBox</button>
+      <button onClick={clickHandler}>Add value = "Utshab"</button>
     </div>
   );
 };
